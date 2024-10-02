ee0be0a94709417f03ce743ad4763572d9d9208c
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-02 09:43:22 +0200

    update tests

--- __tests__/mathFunctions.test.js
+++ __tests__/mathFunctions.test.js
@@ -1,5 +1,5 @@
 // __tests__/mathFunctions.test.js
-const { add } = require("../mathFunctions.js");
+const { add, mod } = require("../mathFunctions.js");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -7,4 +7,8 @@ test("adds 1 + 2 to equal 3", () => {
 
 test("adds -5 + 5 to equal 0", () => {
   expect(add(-5, 5)).toBe(0);
+});
+
+test("check rest from division 10 by 4", () => {
+  expect(mod(10, 4)).toBe(1);
 });
